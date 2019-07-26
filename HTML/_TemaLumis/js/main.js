7212869b0b60bcb49c393bb350e6aba02e0b44d4
egomesmtt <egomes@metatron.com.br>
2019-07-26 15:17:02 -0300

    original

---
+++ HTML/_TemaLumis/js/main.js
@@ -0,0 +1,81 @@
+var body = document.querySelector('body');
+
+function toggleOverlay() {
+  if (document.querySelector('.overlay')) {
+    var overlay = document.querySelector('.overlay');
+    overlay.parentNode.removeChild(overlay);
+  } else {
+    var overlay = document.createElement('div');
+    overlay.setAttribute('class', 'overlay');
+    body.appendChild(overlay);
+  }
+}
+
+function openMenu() {
+  toggleOverlay();
+  body.classList.add('overflow');
+  document.querySelector('.main-menu').style.left = 0;
+  document.querySelector('.overlay').addEventListener('click', closeMenu);
+}
+
+function closeMenu() {
+  toggleOverlay();
+  body.classList.remove('overflow');
+  document.querySelector('.main-menu').style.left = '-285px';
+}
+
+document.querySelector('.menu-close').addEventListener('click', closeMenu);
+document.querySelector('.menu-toggle').addEventListener('click', openMenu);
+
+function openSearch() {
+  var elem = document.querySelector('.form-wrapper');
+  elem.classList.add('open');
+}
+
+function closeSearch() {
+  var elem = document.querySelector('.form-wrapper');
+  elem.classList.remove('open');
+}
+
+document.querySelector('.search-toggle').addEventListener('click', openSearch);
+document.querySelector('.form-close').addEventListener('click', closeSearch);
+
+// Carousel - Galeria de Imagens
+
+var slider = tns({
+  container: '.slider-content',
+  items: 1,
+  navAsThumbnails: true,
+  navContainer: '.nav-wrapper',
+  loop: false
+});
+
+document.querySelector('.total-slides').innerHTML = slider.getInfo().slideCount;
+
+var navWrapper = document.querySelector('.nav-wrapper')
+var width = navWrapper.childElementCount * 91
+navWrapper.setAttribute('style', 'width: ' + width + 'px')
+
+document.querySelector('.tns-controls').addEventListener('click', function() {
+  var currentSlide = slider.getInfo().index + 1;
+  document.querySelector('.current-slide').innerHTML = currentSlide;
+});
+
+// Share Widget
+
+var shareBtn = document.querySelectorAll('.article__share');
+
+shareBtn.forEach(function(element) {
+  element.addEventListener('click', function() {
+    if (navigator.share) {
+      navigator.share({
+        title: pageTitle,
+        url: window.location.href
+      });
+    } else {
+      var socialFallback = element.nextElementSibling;
+      socialFallback.style.display = 'flex';
+      element.style.display = 'none';
+    }
+  });
+})
